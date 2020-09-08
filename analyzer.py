913104f419441c3656a8ae2d79b842c13ccd4b1b
Diana (Noar) Aponte <daponte@picap.co>
2020-09-08 18:01:07 -0500

    Add token regex, ignored and corrections

--- analyzer.py
+++ analyzer.py
@@ -4,7 +4,7 @@ import ply.lex as lex
 lex_result = []
 
 tokens = [
-    'NAME',
+    'IDENTIFIER',
     'NUMBER',
     'PLUS',
     'MINUS',
@@ -13,6 +13,16 @@ tokens = [
     'ASSING',
     'LPAREN',
     'RPAREN',
+    'SINGLE_QUOTE',
+    'QUOTE',
+    'STRING',
+    'POINT',
+    'COMMA',
+    'LBRACKE',
+    'RBRACKE',
+    'END_LINE',
+    'COMMENT',
+    'SPACE'
 ]
 
 t_PLUS = r'\+'
@@ -22,11 +32,18 @@ t_DIVIDE = r'/'
 t_ASSING = r'='
 t_LPAREN = r'\('
 t_RPAREN = r'\)'
-t_NAME = r'[a-zA-Z_][a-zA-Z0-9_]*'
+t_IDENTIFIER = r'[a-zA-Z_][a-zA-Z0-9_]*'
+t_SINGLE_QUOTE = r'\''
+t_QUOTE = r'\"'
+t_POINT = r'\.'
+t_COMMA = r'\,'
+t_LBRACKE = r'\{'
+t_RBRACKE = r'\}'
+t_END_LINE = r';'
 
 
 # Ignored characters
-t_ignore = " \t"
+t_ignore = " [\t]|"
 
 reserved = {
     'if': 'IF',
@@ -37,17 +54,80 @@ reserved = {
     'require': 'REQUIRE',
     'var': 'VAR',
     'let': 'LET',
-    'const': 'const'
+    'const': 'CONST',
+    'function': 'FUNCTION',
+    'break': 'BREAK',
+    'for': 'FOR'
 }
 
 tokens = tokens + list(reserved.values())
 
+def t_COMMENT(t):
+    r'//.*'
+    pass
+
+def t_SPACE(t):
+    r'\n'
+    pass
+
+def t_LET(t):
+    r'let'
+    t.type = reserved.get(t.value, 'LET')
+    return t
+
+def t_ELSE(t):
+    r'else'
+    t.type = reserved.get(t.value, 'ELSE')
+    return t
+
+def t_WHILE(t):
+    r'while'
+    t.type = reserved.get(t.value, 'WHILE')
+    return t
+
+def t_IMPORT(t):
+    r'import'
+    t.type = reserved.get(t.value, 'IMPORT')
+    return t
+
+def t_REQUIRE(t):
+    r'require'
+    t.type = reserved.get(t.value, 'REQUIRE')
+    return t
+
+def t_VAR(t):
+    r'var'
+    t.type = reserved.get(t.value, 'VAR')
+    return t
+
+def t_CONST(t):
+    r'const'
+    t.type = reserved.get(t.value, 'CONST')
+    return t
+
+def t_FUNCTION(t):
+    r'function'
+    t.type = reserved.get(t.value, 'FUNCTION')
+    return t
+
+def t_BREAK(t):
+    r'break'
+    t.type = reserved.get(t.value, 'BREAK')
+    return t
+
+def t_FOR(t):
+    r'for'
+    t.type = reserved.get(t.value, 'FOR')
+    return t
 
 def t_NUMBER(t):
     r'\d+'
     t.value = int(t.value)
     return t
 
+def t_STRING(t):
+    r'(\'([^\'])*\')|(\"([^\"])*\")'
+    return t
 
 def t_error(token):
     global lex_result
